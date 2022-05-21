46dc4fd759d340293fd64e27888c3874c5051895
Digital Mind <jremesz@gmail.com>
2022-05-21 07:53:04 +0200

    initial stage - menu without mobile menu version  and content sections created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        ptsans: ["PT Sans"],
+        playfair: ["Playfair Display"],
+      },
+    },
+  },
+  plugins: [],
+};
